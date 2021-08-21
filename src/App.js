49153e22880b1dfb53bc5dda27acc67a59b962c4
Julia Chen <juliazychen@gmail.com>
2021-08-21 11:32:20 -0400

    Change title

--- src/App.js
+++ src/App.js
@@ -5,19 +5,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Watchr</h1>
       </header>
+      <body>
+        <p>Wow Watchr is such a cool app</p>
+        <ol>
+          <li>Schedule a date and time to record at.</li>
+          <li>Pick a window or screen to record.</li>
+          <li>Confirm your scheduled recording.</li>
+        </ol>
+      </body>
     </div>
   );
 }

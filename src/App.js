cae855f7f09c780eb496c445b6e12ba2f6610ba0
askanniething <39163490+askanniething@users.noreply.github.com>
2021-08-21 20:46:26 -0400

    Added screen capture

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from './components/Form/Form'
 import './App.css';
+
+const electron = window.require("electron");
+
+let mediaRecorder; // MediaRecorder instance to capture footage
+const recordedChunks = [];
+
+const { desktopCapturer, remote } = electron; 
+
+const { writeFile } = window.require('fs');
+
+const { dialog, Menu } = remote;
+
+
+export const Video = ({ srcObject, ...props }) => {
+  const refVideo = React.useCallback(
+    (node) => {
+      if (node) node.srcObject = srcObject;
+    },
+    [srcObject],
+  );
+
+  return <video ref={refVideo} {...props} />;
+};
 
 const gensym = (() => {
     let n = 0;
@@ -13,14 +36,118 @@ const RecordingsContext = React.createContext({
 });
 
 const App = () => {
+
+  const [videoText, changeText] = useState('');
+  const [streamer, changeStream] = useState(null);
+
+const startRec = () => {
+  console.log('dab');
+  mediaRecorder.start();
+  console.log('started');
+}
+
+const stopRec = () => {
+  console.log('dab');
+  mediaRecorder.stop();
+  console.log('stopped');
+}
+
+const getVideoSources = async () => {
+  const inputSources = await desktopCapturer.getSources({
+    types: ['window', 'screen']
+  });
+
+  const videoOptionsMenu = Menu.buildFromTemplate(
+    inputSources.map(source => {
+      return {
+        label: source.name,
+        click: () => selectSource(source)
+      };
+    })
+  );
+  videoOptionsMenu.popup();
+}
+
+// Change the videoSource window to record
+async function selectSource(source) {
+  console.log('reached');
+  changeText(source.name);
+  
+  console.log(videoText);
+
+  const constraints = {
+    audio: false,
+    // {
+    //   mandatory: {
+    //     chromeMediaSource: 'desktop',
+    //     chromeMediaSourceId: source.id
+    //   }
+    // },
+    video: {
+      mandatory: {
+        chromeMediaSource: 'desktop',
+        chromeMediaSourceId: source.id
+      }
+    }
+  };
+
+  // Create a Stream
+  const stream = await navigator.mediaDevices
+    .getUserMedia(constraints);
+
+  // Preview the source in a video element
+  // currentStream = stream;
+  changeStream(stream);
+
+  // videoElement.srcObject = stream;
+  // videoElement.play();
+
+  // Create the Media Recorder
+  const options = { mimeType: 'video/webm; codecs=vp9' };
+  mediaRecorder = new MediaRecorder(stream, options);
+
+  // Register Event Handlers
+  mediaRecorder.ondataavailable = handleDataAvailable;
+  mediaRecorder.onstop = handleStop;
+
+  // Updates the UI
+}
+
+// Captures all recorded chunks
+function handleDataAvailable(e) {
+  console.log('video data available');
+  recordedChunks.push(e.data);
+}
+
+// Saves the video file on stop
+async function handleStop(e) {
+  const blob = new Blob(recordedChunks, {
+    type: 'video/webm; codecs=vp9'
+  });
+
+  const buffer = Buffer.from(await blob.arrayBuffer());
+
+  const { filePath } = await dialog.showSaveDialog({
+    buttonLabel: 'Save video',
+    defaultPath: `vid-${Date.now()}.webm`
+  });
+
+  if (filePath) {
+    writeFile(filePath, buffer, () => console.log('video saved successfully!'));
+  }
+
+}
+
   const [recordings, setRecordings] = React.useState({});
 
   const startCapture = () => {
     console.log('Started capture');
+    startRec();
   };
 
   const stopCapture = () => {
     console.log('Stopped capture');
+    stopRec();
   };
 
   const timeoutDuration = (start, now) => {
@@ -75,6 +202,8 @@ const App = () => {
     setRecordings({...recordings, [id]: undefined});
   }
 
+
+
   return (
     <div className="App">
       <RecordingsContext.Provider value={{ recordings, createRecording, deleteRecording }}>
@@ -99,6 +228,21 @@ const App = () => {
             <p>{recording.type}</p>
           </div>
         ))}
+
+        {/* <video autoPlay>
+          {/* <source src={streamer}></source> 
+
+        </video> */}
+
+        <Video autoPlay srcObject = {streamer} />
+
+        <button id="startBtn" className="button is-primary" onClick = {() => startRec()}>Start</button>
+        <button id="stopBtn" className="button is-warning"  onClick = {() => stopRec()}>Stop</button>
+        <button id="videoSelectBtn" className="button is-text" onClick = {() => getVideoSources()}>
+          {videoText.length === 0 ? 'Choose a Video Source' :  videoText}
+        </button>
+
+
       </RecordingsContext.Provider>
     </div>
   );
--- src/Main.js
+++ src/Main.js
@@ -4,10 +4,19 @@ let win;
 
 function createWindow () {   
   // Create the browser window.     
-  win = new BrowserWindow({width: 800, height: 600}) 
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    }
+  });
+
         
   // and load the index.html of the app.     
-  win.loadURL('http://localhost:3000/');
+  win.loadURL('http://localhost:3001/');
 }      
 
 app.on('ready', createWindow)
